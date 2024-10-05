6d96d68721d3d1381f49e107b5b58f92a910ff4a
Jabbeer Jeerooburkhan <jabbeer@googlemail.com>
2024-10-05 19:07:28 +0100

    Added styling with all correct titles and small scroling images

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "./HomeScreen.css";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import requests from './Requests';
+import Row from './Row';
 
 function HomeScreen() { 
   return (
@@ -9,12 +11,23 @@ function HomeScreen() {
       <Nav />
       
       <Banner />
-    
 
-      {/* Row */}
-
+      <Row 
+        title='NETFLIX ORIGINALS'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      
+      />
+        
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomancenMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;

8c7ecbbf7bb297ebd1c5b890d6e042b62475ae84
Jabbeer Jeerooburkhan <jabbeer@googlemail.com>
2024-10-13 22:05:43 +0100

    Added login screen with styling

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
   return (
     <div className="app">
 
       <BrowserRouter>
-        <Routes>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
           <Route path="/" element={<HomeScreen />}/>
         </Routes>
+        )}
       </BrowserRouter>
-      
+
     </div>
   );
 }

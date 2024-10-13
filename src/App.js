d5f28e35df76b47e0a7796344f8c8b9b8036b2cb
Jabbeer Jeerooburkhan <jabbeer@googlemail.com>
2024-10-13 18:15:50 +0100

    added fire base and deployed app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeScreen />}/>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }

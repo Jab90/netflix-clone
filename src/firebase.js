b2c4c32c19b5ef5bf1fd5683ed2d002bf5c38de7
Jabbeer Jeerooburkhan <jabbeer@googlemail.com>
2024-10-28 17:08:36 +0000

    Issues with firebase, need to update for current firebase version

--- src/firebase.js
+++ src/firebase.js
@@ -20,6 +20,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+
 export { auth, db };
 
 
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -21,6 +21,14 @@ function SignupScreen() {
 
   const signIn = (e) => {
     e.preventDefault();
+
+    auth.signInWithEmailAndPassword(
+      emailRef.current.value,
+      passwordRef.current.value
+    ).then((authUser) => {
+      console.log(authUser);
+    })
+    .catch((error) => alert(error.message));
   };
 
 
